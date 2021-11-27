7054acc9bf6f4408dc73903b835c9f1e3105a75d
Tanja Koivunen <tanja.koivunen@elisanet.fi>
2021-11-27 11:31:26 +0200

    -

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
 const app = express();
-const cors = require('cors');
+const cors = require('cors'); //frontin yhdistämiseen
 
 // parse requests of content-type: application/json
 //app.use(bodyParser.json())
